37f3e53125e1c40dd68742d27ad1262e4a4ad972
Pavel Grynchenko <psdcoder@gmail.com>
2013-07-06 14:25:37 +0300

    Add selection styles and js for selection right tab when user opens page with some hash

---
+++ common-files/dev/js/scripts.js
@@ -0,0 +1,24 @@
+(function($, undefined) {
+    $(function () {
+        var currentHash = window.location.hash,
+            $tabs = $('.tabs-container');
+
+        if (currentHash === '') {
+            return;
+        }
+
+        var $link = $('a[href="' + currentHash + '"]', $tabs);
+
+        if ($link.length > 0) {
+            $link.tab('show');
+        }
+
+        $tabs.on('click', 'a', function() {
+            var hash = $(this).attr('href');
+
+            if (hash !== '') {
+                window.location.hash = hash;
+            }
+        });
+    });
+})(window.jQuery);
